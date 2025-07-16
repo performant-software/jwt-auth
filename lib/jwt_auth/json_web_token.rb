1d90e313e42bfc032be74c785db793986b872288
dleadbetter <derek@performantsoftware.com>
2025-07-16 09:07:07 -0400

    CDC #458 - Updating rails dependency

--- lib/jwt_auth/json_web_token.rb
+++ lib/jwt_auth/json_web_token.rb
@@ -2,7 +2,7 @@ require 'jwt'
 
 module JwtAuth
   class JsonWebToken
-    SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+    SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
 
     def self.encode(payload)
       JWT.encode(payload, SECRET_KEY)
